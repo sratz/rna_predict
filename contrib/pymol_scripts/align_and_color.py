083e244f5d8b89b2e8f5ad46eeeeaae6b6f41b5b
Sebastian Ratz <sebastian.ratz@student.kit.edu>
2015-02-09 16:21:55 +0100

    code style: whitespace

--- contrib/pymol_scripts/align_and_color.py
+++ contrib/pymol_scripts/align_and_color.py
@@ -9,13 +9,15 @@
 
 from pymol import cmd
 
+
 def align_and_color(moving, fixed, maximum=None):
-	alignment_name = "alignment_" + moving
-	cmd.align(moving, fixed, object=alignment_name, cycles=0)
-	cmd.disable(alignment_name)
-	rmsd_b(moving + " & " + alignment_name, fixed + " & " + alignment_name)
-	cmd.color("white", fixed)
-	cmd.spectrum("b", "green_red", moving, 0, maximum)
-	cmd.show("cartoon")
+    alignment_name = "alignment_" + moving
+    cmd.align(moving, fixed, object=alignment_name, cycles=0)
+    cmd.disable(alignment_name)
+    rmsd_b(moving + " & " + alignment_name, fixed + " & " + alignment_name)
+    cmd.color("white", fixed)
+    cmd.spectrum("b", "green_red", moving, 0, maximum)
+    cmd.show("cartoon")
+
 
 cmd.extend("align_and_color", align_and_color)
