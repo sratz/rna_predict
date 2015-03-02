93fc0547de3fbad0af035ef0d76043374ed6c37f
Sebastian Ratz <sebastian.ratz@student.kit.edu>
2015-03-02 16:48:22 +0100

    dcatoos: parse_dca_data: actually skip invalid contacts
    Forgot to actually skip invalid contacts in a62f6bc6037d3c7a36ca3d9bb256adeedea37926.

--- rna_prediction/dcatools.py
+++ rna_prediction/dcatools.py
@@ -294,6 +294,7 @@ def parse_dca_data(dca_prediction_filename):
                 # our dca file might contain more than what we are interested in for the current prediction.
                 # simply ignore all dca pairs that don't fit the mapping range, but print a warning.
                 print "Warning: At least one residue of contact %s not found in PDB-Mapping. Ignoring..." % parts[:2]
+                continue
         else:
             res1 = int(parts[0])
             res2 = int(parts[1])
